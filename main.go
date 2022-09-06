b8b415c8839e6fb91ddb49bc130f0722a8921784
Tanguy ⧓ Herrmann <dolanorgit@evereska.org>
2022-09-06 14:29:37 +0200

    generated

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"context"
+
+	"github.com/dagger/cloak/sdk/go/dagger"
+)
+
+func (r *hugo) generate(ctx context.Context, src dagger.FSID) (*dagger.Filesystem, error) {
+	panic("not implemented")
+}
